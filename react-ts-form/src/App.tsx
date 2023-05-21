921d14b88da2f8595d09ffa66953978e483d535f
mjr2595 <michaeljohnraymond@gmail.com>
2023-05-20 23:41:15 -0500

    format, handleSubmit

--- react-ts-form/src/App.tsx
+++ react-ts-form/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Form from './components/Form'
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-
   return (
     <div>
-      <Form/>
+      <Form />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
