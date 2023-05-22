2a4ede500dc9f90c3cc8260ff4dd74316d575f43
mjr2595 <michaeljohnraymond@gmail.com>
2023-05-21 21:40:43 -0500

    expense list and filter

--- react-ts-form/src/App.tsx
+++ react-ts-form/src/App.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
+    { id: 3, description: "ccc", amount: 10, category: "Entertainment" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expenseSelection) => expenseSelection.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
-      <Form />
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((expenseToDelete) => expenseToDelete.id !== id))}
+      />
     </div>
   );
 }
