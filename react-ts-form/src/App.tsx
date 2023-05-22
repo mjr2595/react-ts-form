43c67f8d3cd521bd88944c8318039d6902584cf2
mjr2595 <michaeljohnraymond@gmail.com>
2023-05-22 00:37:37 -0500

    submit form

--- react-ts-form/src/App.tsx
+++ react-ts-form/src/App.tsx
@@ -4,16 +4,14 @@ import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
 
-export const categories = ["Groceries", "Utilities", "Entertainment"];
-
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
 
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
-    { id: 3, description: "ccc", amount: 10, category: "Entertainment" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+    { id: 1, description: "Gas", amount: 10, category: "Utilities" },
+    { id: 2, description: "HEB", amount: 200, category: "Groceries" },
+    { id: 3, description: "Netflix", amount: 10, category: "Entertainment" },
+    { id: 4, description: "Electricity", amount: 150, category: "Utilities" },
   ]);
 
   const visibleExpenses = selectedCategory
@@ -23,7 +21,9 @@ function App() {
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(newExpense) => setExpenses([...expenses, { ...newExpense, id: expenses.length + 1 }])}
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
